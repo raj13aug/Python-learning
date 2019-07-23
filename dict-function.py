6b4d4558a2fadb72b04c1510df612bbe1d2830fc
Nataraj.R <raj13aug@gmail.com>
2019-07-23 19:41:03 +0530

    23july

---
+++ dict-function.py
@@ -0,0 +1,8 @@
+dict1 = {"Name": "Nataraj", "Age": 22}
+print(dict1)
+print(type(dict1))
+del dict1["Name"]
+print(dict1)
+dict1["Name"] = "Nataraj"
+print(dict1)
+print("There A name key:", "Name" in dict1)
