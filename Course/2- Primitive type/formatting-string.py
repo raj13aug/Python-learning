5b03171163ee83550f89fd6a63ab562ee295c81f
raj13aug <raj13aug@yahoo.in>
2020-01-07 09:03:57 +0530

    Create formatting-string.py

---
+++ Course/2- Primitive type/formatting-string.py
@@ -0,0 +1,17 @@
+
+# concatenation to build a string
+
+frist = "mosh"
+last = "Hamedani"
+full = frist + " " + last
+print(full)
+
+# #formatted string, you can put any valid expressions
+
+
+frist = "mosh"
+last = "Hamedani"
+full = f"{frist} {last}"
+full = f"{len(frist)} {last}"
+full = f"{len(frist)} {2 + 2}"
+print(full)
