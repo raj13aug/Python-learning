a0f9bc7f96885117bfa352961882731ecb81dfd6
raj13aug <raj13aug@yahoo.in>
2020-01-10 08:57:59 +0530

    Create Type_of_converstion.py

---
+++ Course/2- Primitive type/Type_of_converstion.py
@@ -0,0 +1,27 @@
+
+# All is built in function
+
+# int(x)
+# float(x)
+# bool(x)
+# str(x)
+
+
+# print(type(x)) check the type of value. whether string or float
+
+# Boolean
+# "" -> emtmy string considered as falsey
+# 0 is also falsey,
+# None
+
+x = input("x: ")        # Input value always coming from sting value, so we have to convert the integer value.
+print(type(x))
+y = int(x) + 1
+print(f"x: {x}, y: {y}")
+
+print(bool(0))  # -> False
+print(bool(1))  # -> True
+print(bool(-1))  # -> True
+print(bool(5))  # -> True
+print(bool(""))  # -> False
+print(bool("False"))  # -> True
