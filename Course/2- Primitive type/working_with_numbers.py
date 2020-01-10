328d3bfc234e8b84acdbe2aebb2ee0e4230fd307
raj13aug <raj13aug@yahoo.in>
2020-01-10 08:36:52 +0530

    Create working_with_numbers.py

---
+++ Course/2- Primitive type/working_with_numbers.py
@@ -0,0 +1,9 @@
+import math
+
+print(round(2.9))  # for rounding a number,
+print(abs(-2.9))  # absolute value of a number
+
+# Mathematical calculations -> use the math module.
+print(math.ceil(2.2))
+
+# Document --> https://docs.python.org/3/library/math.html
