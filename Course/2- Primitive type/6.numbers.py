1b1a2e0c631c908c311345034ea463ca49e460b6
raj13aug <raj13aug@yahoo.in>
2020-01-10 08:27:04 +0530

    Create 6.numbers.py

---
+++ Course/2- Primitive type/6.numbers.py
@@ -0,0 +1,16 @@
+print(10 + 3)  # -> Addition
+print(10 - 3)  # -> subtraction
+print(10 * 3)  # -> multiplication
+
+# Two different of division
+
+print(10 / 3)  # -> division operator get a floating point number.
+print(10 // 3)  # ->  If you want an integer, you use double slashes.
+
+print(10 % 3)  # modulus which is remainder of a division,
+print(10 ** 3)  # exponent is left to the power of right.
+
+# Augmented assignment operator.
+x = 10
+x = x + 3  # you want to increment 10 by 3
+x += 3    # assignment operator
