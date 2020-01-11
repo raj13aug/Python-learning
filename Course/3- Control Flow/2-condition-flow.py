dded2f6ddddec3ac178f10ca38e3c0dc968fe49f
raj13aug <raj13aug@yahoo.in>
2020-01-11 18:30:04 +0530

    Create 2-condition-flow.py

---
+++ Course/3- Control Flow/2-condition-flow.py
@@ -0,0 +1,9 @@
+temperture = 1
+if temperture > 30:
+    print(" It's Warm")
+    print("Drink Water")  # Pay great attention to these indentations
+elif temperture > 10:
+    print("It's nice")
+else:
+    print("It's cold")
+print("Done")
