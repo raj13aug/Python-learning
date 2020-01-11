d0ffbd14ad979dadb40c879afc87cfd46e4bf385
raj13aug <raj13aug@yahoo.in>
2020-01-11 18:39:02 +0530

    Create 3-Ternary-operator.py

---
+++ Course/3- Control Flow/3-Ternary-operator.py
@@ -0,0 +1,18 @@
+age = 22
+if age >= 18:
+    print("Eligible")
+else:
+    print("Not Eligible")
+
+###############################
+
+if age >= 18:
+    message = "Eligible"
+else:
+    message = "Not Eligible"
+print(message)
+
+#################################
+
+message = "Eligible" if 18 >= 20 else "Not Eligible"
+print(message)
