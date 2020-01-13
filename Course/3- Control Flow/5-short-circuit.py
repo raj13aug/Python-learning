a52ec02126e3b0469e9fc7f3ecfaedaa3e55f939
raj13aug <raj13aug@yahoo.in>
2020-01-13 22:47:41 +0530

    Update 5-short-circuit.py

--- Course/3- Control Flow/5-short-circuit.py
+++ Course/3- Control Flow/5-short-circuit.py
@@ -1,4 +1,4 @@
-The evaluation stops frist opeartor(high_income) has false is called as short circuit.
+The evaluation stops frist opeartor(high_income) has false on AND opertor is called as short circuit.
 
 if (high_income and  good_credit) and not student:
     print("Eligible")
