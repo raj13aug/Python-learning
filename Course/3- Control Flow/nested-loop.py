89f0b18008da091a5703d6b4ffd7ee46503a18dc
raj13aug <raj13aug@yahoo.in>
2020-01-20 07:47:29 +0530

    Create nested-loop.py

---
+++ Course/3- Control Flow/nested-loop.py
@@ -0,0 +1,5 @@
+for x in range(5):
+    for y in range(5):
+        print(f"({x} {y})")
+
+# Here we called as two loop, outer loop and inner loop. The program start first iteration of outer loop then going to inner loop.
