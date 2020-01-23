f1294a34fe41c4ec658212745e1bedc6e6cec00f
raj13aug <raj13aug@yahoo.in>
2020-01-23 08:23:38 +0530

    Create 2-argument.py

---
+++ Course/4-functions/2-argument.py
@@ -0,0 +1,7 @@
+#A parameter is the input that you define for your function,whereas an argument is that actual value for a given parameter.
+def greet(first_name, last_name):
+    print(f"Hi {first_name} {last_name}")
+    print("Welcome aboard")
+
+
+greet("Nataraj", "Ramamoorthy")
