054b109f8da6b11a568741d4a73b1b45cfefc524
raj13aug <raj13aug@yahoo.in>
2020-01-24 23:32:09 +0530

    Create Keyboard-argument.py

---
+++ Course/4-functions/Keyboard-argument.py
@@ -0,0 +1,10 @@
+def increment(number, by):
+    return number + by
+
+
+result = increment(2, 1)
+print(result)
+# simpliyfied  the code
+print(increment(3, 5))
+# code more readable by keyboard arguments
+print(increment(4, by=9))
