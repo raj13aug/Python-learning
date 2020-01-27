73510bb20326329f7bd24294068fa518e31979a1
raj13aug <raj13aug@yahoo.in>
2020-01-27 07:11:38 +0530

    Create debug.py

---
+++ Course/4-functions/debug.py
@@ -0,0 +1,17 @@
+def multiply(*numbers):
+    total = 1
+    for number in numbers:
+        total *= number
+        return total              # debug the code
+
+
+print("start")
+print(multiply(1, 2, 3))
+
+
+
+#Break point apply for code : press f9
+#continous to execute the code: f10
+#show the variable the problem : f11
+
+shift + f11 --> 
