d6d67141b9a9322e1b03115d59a5488a8b6b199a
raj13aug <raj13aug@yahoo.in>
2020-02-01 08:14:50 +0530

    Update Adding-removing-items.py

--- Course/5-Data-Structure/Adding-removing-items.py
+++ Course/5-Data-Structure/Adding-removing-items.py
@@ -1,1 +1,32 @@
+letters = ["a", "b", "c"]
+# We use the append method to add something at the end of this list
+letters.append("a")
+print(letters)
+# you want to add specific position you should use the insert methond
+letters.insert(0, "3")
+print(letters)
+
+
+# Remove
+# Pop method removing the end of list
+letters.pop()
+print(letters)
+# use index also
+letters.pop(0)
+print(letters)
+# Remove method removing charater in first
+letters.remove("b")
+print(letters)
+
+# Delete the range of iteams
+del letters[0:1]
+print(letters)
+
+# different between pop and del methods
+# The pop method will remove only one item by index
+# Delete method we remove range of items
+
+# clear all object in list
+letters.clear()
+print(letters)
 
