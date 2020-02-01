8d51f414fe4d069f68c2f031a2b330ff430467c2
raj13aug <raj13aug@yahoo.in>
2020-02-01 07:39:50 +0530

    Update List-unpacking.py

--- Course/5-Data-Structure/List-unpacking.py
+++ Course/5-Data-Structure/List-unpacking.py
@@ -1,1 +1,19 @@
-1
+numbers = [1, 2, 3]
+# Should be equal to the number of iteams we have in the list
+first, second, third = numbers
+# Above and below both are same.
+first = numbers[0]
+second = numbers[1]
+third = numbers[2]
+
+numbers = [1, 2, 3, 4, 5, 6]
+
+# iteams and everthing will be stored in a separate list called other.
+first, second, *others = numbers
+print(first)
+print(second)
+print(others)
+
+first, *others, last = numbers  # you can get last value using last
+print(first, last)
+print(others)
