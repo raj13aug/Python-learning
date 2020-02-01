377cd3208c7ccef99473b3abaf4ba18252906218
raj13aug <raj13aug@yahoo.in>
2020-02-01 07:54:59 +0530

    Update 4-looping-over-lists.py

--- Course/5-Data-Structure/4-looping-over-lists.py
+++ Course/5-Data-Structure/4-looping-over-lists.py
@@ -1,1 +1,25 @@
-1
+letters = ["a", "b", "c"]
+for letter in letters:
+    print(letter)
+
+
+#
+letters = ["a", "b", "c"]
+for letter in enumerate(letters):
+    print(letter)
+
+# you will get index value
+letters = ["a", "b", "c"]
+for letter in enumerate(letters):
+    print(letter[0])
+
+# you will get index value and list value
+letters = ["a", "b", "c"]
+for letter in enumerate(letters):
+    print(letter[0], letter[1])
+
+#
+for index, letter in enumerate(letters):
+    print(index, letter)
+# enumerate function returns an enumerate object which is iterable. The enumerate object will return a tuple that looks like this
+
