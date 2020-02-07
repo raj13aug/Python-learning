30e4dc93aa30017261d931a5e6a7bb6c55ad43f5
raj13aug <raj13aug@yahoo.in>
2020-02-07 19:22:53 +0530

    Create swapping-variable.py

---
+++ Course/5-Data-Structure/swapping-variable.py
@@ -0,0 +1,20 @@
+x = 10
+y = 20
+
+# Here We can assign the additional variable to shift the variable
+
+c = x
+x = y
+y = c
+
+print("x", x)
+print("y", y)
+
+# It's another method to swapping the variable
+x, y = y, x
+
+# We can assign variables   
+
+x, y = 1, 2
+
+x, y, z = point
