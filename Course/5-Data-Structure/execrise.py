05253b6941d1b4ec197324df3d19bfd1c15d93e8
raj13aug <raj13aug@yahoo.in>
2020-02-12 14:09:43 +0530

    Update execrise.py

--- Course/5-Data-Structure/execrise.py
+++ Course/5-Data-Structure/execrise.py
@@ -1,6 +1,8 @@
 from pprint import pprint
 sentence = " This is a comment question"
 
+# Dictionaries lije sets, are unordered collection, we cannot sort them, we can only sort lists
+
 char_frequency = {}
 for char in sentence:
     if char in char_frequency:
@@ -9,3 +11,6 @@ for char in sentence:
         char_frequency[char] = 1
 
 pprint(char_frequency, width=1)
+
+char_frequency_sorted = sorted(char_frequency.items(), key=lambda kv:kv [1], reverse=True)
+print(char_frequency_sorted[0])
