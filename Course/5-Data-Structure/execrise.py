cf9429697f4ba290976e7e2322a635defc8c89b7
raj13aug <raj13aug@yahoo.in>
2020-02-12 12:43:29 +0530

    Create execrise.py

---
+++ Course/5-Data-Structure/execrise.py
@@ -0,0 +1,11 @@
+from pprint import pprint
+sentence = " This is a comment question"
+
+char_frequency = {}
+for char in sentence:
+    if char in char_frequency:
+        char_frequency[char] += 1
+    else:
+        char_frequency[char] = 1
+
+pprint(char_frequency, width=1)
