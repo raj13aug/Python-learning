ec0f1fd7124dd66a4585a9f0b66418b3c203324d
Nataraj.R <raj13aug@gmail.com>
2020-02-13 06:55:41 +0530

    created folder

--- Course/exception.py
+++
@@ -1,3 +0,0 @@
-age = int(input("age: "))
-
-# if you pass the float value then showing as invalid error. so you should take the responsible for thing. 
--- Course/handing-execption.py
+++
@@ -1,9 +0,0 @@
-try:
-    age = int(input("age: "))
-except ValueError as ex:
-    print("you didn't enter a vaild age.")
-    print(ex)
-    print(type(ex))
-else:
-    print("No exception were thrown")
-print("Execution continous")
