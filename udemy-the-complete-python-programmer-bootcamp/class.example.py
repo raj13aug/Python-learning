49b7016a13024349e38d18fc9ffa42bd69b3e179
Nataraj Ramamoorthy <raj13aug@yahoo.in>
2020-02-15 12:15:32 +0530

    Update class.example.py

--- udemy-the-complete-python-programmer-bootcamp/class.example.py
+++ udemy-the-complete-python-programmer-bootcamp/class.example.py
@@ -1,7 +1,7 @@
 class Patient(object):
     ''' Medical Bill'''
-    status = 'Patient'
-    def __init__(self, name, age):
+    status = 'Patient'             # class variable
+    def __init__(self, name, age):     # instance variable
         self.name = name
         self.age = age
 
@@ -9,9 +9,11 @@ class Patient(object):
 patient = Patient('Nataraj', 32)
 print(patient.name)
 print(patient.age)
+print(patient.status)
 patient = Patient('vinoth', 35)
 print(patient.name)
 print(patient.age)
+print(patient.status)
 
 # We have instance variables that are created by the constructor we don't know what each instance is going to be called.
 
