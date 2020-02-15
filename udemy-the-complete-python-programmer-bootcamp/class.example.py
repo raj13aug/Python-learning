83fa6df74df2054caaf3f6845a2278d8ca2a1eea
Nataraj Ramamoorthy <raj13aug@yahoo.in>
2020-02-15 12:14:17 +0530

    Update class.example.py

--- udemy-the-complete-python-programmer-bootcamp/class.example.py
+++ udemy-the-complete-python-programmer-bootcamp/class.example.py
@@ -1,6 +1,6 @@
 class Patient(object):
     ''' Medical Bill'''
-
+    status = 'Patient'
     def __init__(self, name, age):
         self.name = name
         self.age = age
@@ -12,3 +12,7 @@ print(patient.age)
 patient = Patient('vinoth', 35)
 print(patient.name)
 print(patient.age)
+
+# We have instance variables that are created by the constructor we don't know what each instance is going to be called.
+
+# Member every object in this class has irrespective 
