96338893b16982803d80325c3e04ca87b79fdd42
Nataraj Ramamoorthy <raj13aug@yahoo.in>
2020-02-17 23:34:31 +0530

    Update private-member.py

--- Course/7-classes/private-member.py
+++ Course/7-classes/private-member.py
@@ -2,3 +2,6 @@ Make private of self.tags from the outside you need to preface it with 2 underli
 
 print(cloud.__dict__)
 print(cloud._TagCloud__tags)
+
+
+The private members, these private members are still accessible from the outside.
