b2ab206a49bd4ea5920342e093c10de1e3224546
Nataraj Ramamoorthy <raj13aug@yahoo.in>
2020-02-28 08:39:05 +0530

    Create intra-package.py

---
+++ Course/8-modules/intra-package.py
@@ -0,0 +1,19 @@
+If want to import modules from sibling packages.
+
+# ecommerce
+#    --> customer
+#          --> contact.py
+#          --> __init__.py
+#    --> shopping
+#          --> __init__.py
+#          --> sales.py
+
+
+# two types of modules
+
+#     --> absoute import
+#     --> relative import
+
+
+from ecommerce.customer import contact
+form ..customer import contact
