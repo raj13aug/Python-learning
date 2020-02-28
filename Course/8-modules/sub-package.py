1dae638762ac1cca4c6f00f2cb7f8ec217a4ed56
Nataraj Ramamoorthy <raj13aug@yahoo.in>
2020-02-28 08:27:26 +0530

    Create sub-package.py

---
+++ Course/8-modules/sub-package.py
@@ -0,0 +1,4 @@
+# For example: lets imagine our ecommerce package has grown a lot. we have so many files and module here. so we decide to break this package into a few sub package
+
+
+from ecommerce.shopping import sales
