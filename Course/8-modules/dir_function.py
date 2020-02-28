ee174a2927e51f7b892334d09b3417a6faacda40
Nataraj Ramamoorthy <raj13aug@yahoo.in>
2020-02-28 08:44:39 +0530

    Create dir_function.py

---
+++ Course/8-modules/dir_function.py
@@ -0,0 +1,9 @@
+
+from ecommerce.customer import contact
+# dir function, we can get the list of attributes and method defined in an objects
+
+print(dir(sales))
+
+print(sales.__name__)
+print(sales.__package__)
+print(sales.__file__)
