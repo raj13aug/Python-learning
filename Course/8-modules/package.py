dbb65e942e256f5f04e851fa121fc99be159d34e
Nataraj Ramamoorthy <raj13aug@yahoo.in>
2020-02-28 08:23:36 +0530

    Create package.py

---
+++ Course/8-modules/package.py
@@ -0,0 +1,11 @@
+
+
+
+#you have add a new file called double underscore init doule __init__.py. python treat as commerce folder as a package.
+#Package is container for one or more modules.
+#In file sytem the package is mapped to a directory and a module is mapped to a file.
+
+from ecommerce import sales
+
+calc_tax()
+ 
