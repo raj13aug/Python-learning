4c80eed8ef264920f9336875adec5f0f80a432f5
Nataraj Ramamoorthy <raj13aug@yahoo.in>
2020-02-28 08:14:31 +0530

    Create module-search.py

---
+++ Course/8-modules/module-search.py
@@ -0,0 +1,8 @@
+import sales
+import sys
+
+print(sys.path)
+
+# sales.py doesn't find this file, it will look in a bunch of predefined directories that come with python installations.
+
+# import items search these directories to find this module if finds the module, our search stops
