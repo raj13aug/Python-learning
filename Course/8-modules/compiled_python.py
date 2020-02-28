19198e55fc898d04e9a525b56e009a4c30cdac76
Nataraj Ramamoorthy <raj13aug@yahoo.in>
2020-02-28 08:07:49 +0530

    Create compiled_python.py

---
+++ Course/8-modules/compiled_python.py
@@ -0,0 +1,2 @@
+# Python stores these compiled files in this stored folder is to speed up module loading.python will simply load that compiled version.
+# so skip that compilation step, an dthis will result in faster loading of the sales module.only speed up the loading of this module, not the actual performance of the application
