b13a4491cdb80f110e1856f4e5e9eb839e9585be
Nataraj Ramamoorthy <raj13aug@yahoo.in>
2020-03-14 08:58:47 +0530

    Update datetime.py

--- Course/9-python-libary/datetime.py
+++ Course/9-python-libary/datetime.py
@@ -1,6 +1,6 @@
-#dt = datetime.datetime(2018, 1, 1)  --> (datetime.datetime) look ugly, so is better to import the data time class from this module.dont repeat datetime mutiple time.
-#datetime.strptime --> converting string into a datatime object(Particularly useful when we get input from the user or read it from the file.)
-#strftime --> convert a data time object into string
+# dt = datetime.datetime(2018, 1, 1)  --> (datetime.datetime) look ugly, so is better to import the data time class from this module.dont repeat datetime mutiple time.
+# datetime.strptime --> converting string into a datatime object(Particularly useful when we get input from the user or read it from the file.)
+# strftime --> convert a data time object into string
 
 from datatime import datatime
 import time
