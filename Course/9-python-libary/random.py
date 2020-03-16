276e4a3ca9ca237a933540195968369376c32227
Nataraj Ramamoorthy <raj13aug@yahoo.in>
2020-03-16 08:23:04 +0530

    Create random.py

---
+++ Course/9-python-libary/random.py
@@ -0,0 +1,11 @@
+import random
+import string
+
+print(random.random())
+print(random.randint(1, 10))
+print(random.choice([1, 2, 3, 4]))
+print(random.choices([1, 2, 3, 4], k=2))
+print("".join(random.choices(string.ascii_letters + string)))
+
+numbers = [1, 2, 3, 4]
+random.shuffle(numbers)
