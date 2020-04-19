b4f9ca96b7975a6bcedb452e8999b0af4de3e21a
Nataraj Ramamoorthy <raj13aug@yahoo.in>
2020-04-19 07:25:49 +0530

    Create  2- VariableNames.py

---
+++ Course/2- Primitive type/2- VariableNames.py
@@ -0,0 +1,5 @@
+4 things should be use in variable Name.
+
+1. var = "text"  --> remove the space between the value and variable.
+2. course_python --> Alway use underscore between the variable name.
+3. Course_Pyhon or COURSE --> Dont' use any capital lettet.
