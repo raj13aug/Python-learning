83a972251051ec5bd8b8ecd1d0152c9cc769e929
Nataraj Ramamoorthy <raj13aug@yahoo.in>
2020-06-05 20:41:30 +0530

    Create range.py

---
+++ udemy-the-complete-python-programmer-bootcamp/range.py
@@ -0,0 +1,9 @@
+#range you will come across start, stop and step.
+# Start --> value lets you control from where the range begins.
+# Stop --> value lets you control when the range ends.
+# step --> value lets you control how to range is generated
+
+print(list(range(100, 0, -1)))
+print(list(range(5,10)))
+print(list(range(10, 0, -2))) #Range direction by negative
+print(list(range(0, 10, -2))) # O/p get empty list. because start value from 10 and end value is zero. 
