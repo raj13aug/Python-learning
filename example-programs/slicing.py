03df45e5791d8988990bf83531bee0ebe0b6eb3c
Nataraj Ramamoorthy <raj13aug@yahoo.in>
2020-06-11 07:36:27 +0530

    Create slicing.py

---
+++ example-programs/slicing.py
@@ -0,0 +1,12 @@
+book = "The Hitchhiker's guide to the galaxy"
+# variable convert into list
+booklist = list(book)
+
+print(booklist[0:3])
+
+print(''.join(booklist[0:3]))
+print(''.join(booklist[-6:]))
+
+ss = "Sammy Shark!"
+ss = list(ss)
+print(ss[-6:-1])
