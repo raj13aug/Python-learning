5425e6dc8ffe14b449e3cd6986eb132a9888c160
Nataraj Ramamoorthy <raj13aug@yahoo.in>
2020-06-11 17:56:35 +0530

    Update creating-class.py

--- Course/7-classes/creating-class.py
+++ Course/7-classes/creating-class.py
@@ -6,7 +6,12 @@
 
 # All function in our classes should have at least one parameter.
 # If use dot operator, you can see we have the draw method as well as a bunch of other methods that we didn't define. Through a mechanism called inheritance.
+
 # isinstance, sometimes we have an object,if this object is an intance of a given class
+#The isinstance() function checks if the object (first argument) is an instance or subclass of classinfo class (second argument).
+
+#The syntax of isinstance() is:
+#isinstance(object, classinfo)
 
 # Point object need inital values like x and y, to set these values,we need a constructor
 
