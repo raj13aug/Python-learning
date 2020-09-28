a51af1e816545cbd7b1f888dfd64416e6867547d
Nataraj Ramamoorthy <raj13aug@yahoo.in>
2020-09-28 22:33:42 +0530

    Update comprehensions.py

--- Course/5-Data-Structure/comprehensions.py
+++ Course/5-Data-Structure/comprehensions.py
@@ -8,5 +8,8 @@ items = [
 prices = list(map(lambda item: item[1], items))
 prices = [item[1] for item in items]
 
+# explanation:
+# expression for item in items
+
 filtered = list(filter(lamnda item: item[1] >= 10, items))
 filtered = [item for item in items if item[1] >= 10]
